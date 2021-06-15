da0c7f644e43bfd1f051844fe90566113e38d302
chrissyy99 <43761702+IslipAI@users.noreply.github.com>
2021-06-15 18:18:56 -0400

    Added Login Functionality. Still needs work.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import React, { useState } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import './App.css';
+import Beats from './components/Beats/Beats.js';
+import Events from './components/Events/Events.js'
+import Login from './components/Login/Login.js'
 
 function App() {
+  const [token, setToken] = useState();
+
+ if(!token){
+   return <Login setToken={setToken}/>
+ }
+
   return (
-    <div className="App">
-      hey
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/beats">
+            <Beats/>
+          </Route>
+          <Route path="/events">
+            <Events/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
 
-export default function Login() {
+import '../Login/Login.css'
+
+
+async function loginUser(email, password) {
+  console.log('http://www.beatcaveapi.com/login/'.concat(email,'/', password))
+  const token = await fetch('http://www.beatcaveapi.com/login/'.concat(email,'/', password))
+  .then(response => {
+    return response.json();
+  }).then(token =>{
+    console.log(token)
+    return token;
+  });
+
+  return token;
+
+  //console.log(fetchPromise)
+ }
+
+export default function Login({setToken}) {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+
+  const handleSubmit = async e => {
+    const count = 0;
+    e.preventDefault();
+    const token = await loginUser(
+      email,
+      password, 
+    );
+    if(token.length == 1){
+      setToken(token);
+    }
+  }
+
+
   return(
     <div className="login-wrapper">
         <h3>Please Log In</h3>
-        <form>
+        <form onSubmit={handleSubmit}>
         <label>
-            <p>Username</p>
-            <input type="text" />
+            <p>Email</p>
+            <input type="text" onChange={e => setEmail(e.target.value)}/>
         </label>
         <label>
             <p>Password</p>
-            <input type="password" />
+            <input type="password" onChange={e => setPassword(e.target.value)}/>
         </label>
         <div>
             <button type="submit">Submit</button>
@@ -19,4 +55,8 @@ export default function Login() {
         </form>
     </div>
   )
+}
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired
 }
