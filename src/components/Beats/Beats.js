90b0b6d941e3291b05e9ee06b7a5034702a3f66c
chrissyy99 <43761702+IslipAI@users.noreply.github.com>
2021-06-15 12:22:25 -0400

    First Commit
    Initialized the project and deleted useless files, and added directories for organization.

---
+++ src/components/Beats/Beats.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Beats() {
+  return(
+    <h2>Beats</h2>
+  );
+}
---
+++ src/components/Events/Events.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Events() {
+  return(
+    <h2>Events</h2>
+  );
+}
