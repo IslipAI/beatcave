13bcf8a7b64d2c2d285400d44bc51adedaa32289
chrissyy99 <43761702+IslipAI@users.noreply.github.com>
2021-09-27 20:04:11 -0400

    Added event creation form.
    Need to attach there respective post request to the forms.

--- src/components/Views/Events/Events.js
+++ src/components/Views/Events/Events.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default function Events() {
-  return(
-    <h2>Events</h2>
-  );
+
+function EventsDisplay(props){
+  console.log(props.events[0])
+  return props.events.map((events, index) =>{
+      return(
+        <div className=""  key={index}>
+            <p>{props.events[index].name}</p>
+            <p>{props.events[index].venue}</p>
+            <p>{props.events[index].venueaddress}</p>
+            <p>{props.events[index].date.split('T')[0]}</p>
+        </div>
+      )
+    })
+}
+
+
+export default class Events extends Component{
+  constructor(props){
+      super(props);
+      this.state = {
+          error: null, 
+          isLoaded: false, 
+          events: []
+      };
+    }
+
+  //Calls API to get user products by id
+  async fetchAPI(){
+      await fetch("http://localhost:8000/events/")
+      .then(response => {
+          return response.json()
+      })
+      .then(
+          (result) => {
+              this.setState({
+                  isLoaded : true,
+                  events: result.elements
+              });
+              console.log(result.elements);
+  
+          },
+          (error) =>{
+              this.setState({
+                  isLoaded: true, 
+                  error
+              });
+          }
+      )
+  }
+
+  componentDidMount(){
+    this.fetchAPI();
+  }
+  
+  render(){
+      return(
+          <div>
+              <h1>Events</h1>
+              <EventsDisplay events={this.state.events}/>
+          </div>
+      )
+  }
 }
--- src/components/Views/Products/Products.js
+++ src/components/Views/Products/Products.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react';
 
+async function PostEvent(name){
+    console.log("EVENT POST REQUEST!")
+    const requestOptions = {
+      method: 'POST',
+      headers: 
+      { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ 
+          name: name,
+      })
+    }
+}
+async function PostBeat(){
+    console.log("BEAT POST REQUEST!")
 
-function BeatUploadForm(){
+}
+
+function BeatUploadForm(props){
     return(
         <div>
-            <h1>Selling On The Cave?</h1>
-            <form>
+            <form onSubmit={props.uploadProduct}>
             <select id="Genre" name="Genre">
                 <option value="rap">Rap</option>
                 <option value="house">House</option>
@@ -34,7 +48,137 @@ function BeatUploadForm(){
     )
 }
 
-function UploadedBeatsDisplay(props){
+function EventUploadForm(props){
+    return(
+        <div>
+            <form onSubmit={props.uploadProduct}>
+            <input 
+                type="text" 
+                name="name"
+                placeholder="Name"
+                onChange={props.handleChange}
+                required
+            />
+            <br/>
+            <input 
+                type="number" 
+                name="tickets"
+                id="tickets" 
+                min="0" max="1000" 
+                placeholder="Tickets"
+                onChange={props.handleChange}
+                required
+            />
+            <br/>
+            <input 
+                type="text" 
+                name="venuename"
+                placeholder="Venue Name"
+                onChange={props.handleChange}
+                required
+            />
+            <br/>
+            <input 
+                type="text" 
+                name="venueaddress"
+                placeholder="Venue Address"
+                onChange={props.handleChange}
+                required
+            />
+            <br/>
+            <input 
+                type="text" 
+                name="city"
+                placeholder="City"
+                onChange={props.handleChange}
+                required
+            />
+            <br/>
+            <textarea 
+                id="description" 
+                name="description" 
+                rows="5" 
+                cols="30" 
+                placeholder="Description"
+                onChange={props.handleChange}
+                required
+            />
+            <br/>
+            <input 
+                type="number" 
+                name="price"
+                min="1" 
+                max="1000" 
+                step="any"
+                onChange={props.handleChange}
+                required
+            />
+            <br/>
+            <input 
+                type="date" 
+                name="birthdate"
+                onChange={props.handleChange}
+                required
+              />
+            <br/>
+            <select id="starttime" name="starttime" onChange={props.handleChange}>
+                <option value="8AM">8AM</option>
+                <option value="9AM">9AM</option>
+                <option value="10AM">10AM</option>
+                <option value="11AM">11AM</option>
+                <option value="12AM">12PM</option>
+                <option value="1PM">1PM</option>
+                <option value="2PM">2PM</option>
+                <option value="3PM">3PM</option>
+                <option value="4PM">4PM</option>
+                <option value="5PM">5PM</option>
+                <option value="6PM">6PM</option>
+                <option value="7PM">7PM</option>
+                <option value="8PM">8PM</option>
+                <option value="9PM">9PM</option>
+                <option value="10PM">10PM</option>
+                <option value="11PM">11PM</option>
+                <option value="12AM">12AM</option>
+            </select>
+            <select id="endtime" name="endtime" onChange={props.handleChange}>
+                <option value="8AM">8AM</option>
+                <option value="9AM">9AM</option>
+                <option value="10AM">10AM</option>
+                <option value="11AM">11AM</option>
+                <option value="12PM">12PM</option>
+                <option value="1PM">1PM</option>
+                <option value="2PM">2PM</option>
+                <option value="3PM">3PM</option>
+                <option value="4PM">4PM</option>
+                <option value="5PM">5PM</option>
+                <option value="6PM">6PM</option>
+                <option value="7PM">7PM</option>
+                <option value="8PM">8PM</option>
+                <option value="9PM">9PM</option>
+                <option value="10PM">10PM</option>
+                <option value="11PM">11PM</option>
+                <option value="12AM">12AM</option>
+            </select>
+            <br/>
+            <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+function UploadForm(props){
+    if(props.showForm === "beats"){
+        return(
+            <BeatUploadForm handleChange={props.handleChange} uploadProduct={props.uploadProduct}/>
+        )
+    }else{
+        return(
+            <EventUploadForm handleChange={props.handleChange} uploadProduct={props.uploadProduct}/>
+        )
+    }
+}
+
+function UploadedProductsDisplay(props){
     console.log(props.products[0])
     return props.products.map((products, index) =>{
         return(
@@ -53,11 +197,23 @@ export default class Products extends Component{
     constructor(props){
         super(props);
         this.state = {
-            error: null, 
-            isLoaded: false, 
-            products: []
+            products: [], 
+            admin: false,
+            showForm: "beats",
+            name: "", 
+            tickets: 0,
+            venuename: "", 
+            venueaddress: "", 
+            city: "",
+            description: "", 
+            price: 0,
+            date: null, 
+            starttime: null,
+            endtime: null,
         };
-      }
+        this.handleChange = this.handleChange.bind(this);
+        this.uploadProduct = this.uploadProduct.bind(this);
+    }
 
     //Calls API to get user products by id
     async fetchAPI(id){
@@ -97,19 +253,79 @@ export default class Products extends Component{
     }
 
 
+    checkAdmin(){
+        const storedToken = sessionStorage.getItem('token');
+        if(storedToken != null){
+            var tokenBody = storedToken.split('.')[1];
+            //console.log(tokenBody);
+
+            var tokenBodyDecoded = Buffer.from(tokenBody, 'base64').toString();
+            //console.log(tokenBodyDecoded);
+
+            const tokenBodyJson = JSON.parse(tokenBodyDecoded);
+            //console.log(tokenBodyJson.admin);
+
+            if(tokenBodyJson.admin === "A"){
+                this.setState({
+                    admin: true
+                });
+            }else{
+                this.setState({
+                    admin: false
+                });
+            }
+        }
+    }
+
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+        console.log(event.target.value);
+    }
+
+
     componentDidMount(){
         var id = this.getUserId();
         this.fetchAPI(id)
+        this.checkAdmin();
     }
 
-    
+    uploadProduct(e){
+        e.preventDefault();
+        const {name} = this.state;
+        if(this.state.showForm === "beats"){
+            PostBeat();
+        }else{
+            PostEvent();
+        }
+
+    }
+
     render(){
-        return(
-            <div>
-                <h1>Products</h1>
-                <BeatUploadForm/>
-                <UploadedBeatsDisplay products={this.state.products}/>
-            </div>
-        )
+        if(this.state.admin === true){
+            return(
+                <div>
+                    <h1>Products</h1>
+                    <h2>Selling On The Cave?</h2>
+                    <select onChange={this.handleChange} name="showForm">
+                        <option value="beats">Beats</option>
+                        <option value="events">Events</option>    
+                    </select>
+                    <UploadForm showForm={this.state.showForm} handleChange={this.handleChange} uploadProduct={this.uploadProduct}/>
+                    {/* <UploadedProductsDisplay products={this.state.products}/> */}
+                </div>
+            )
+        }else{
+            return(
+                <div>
+                    <h1>Products</h1>
+                    <h2>Selling On The Cave?</h2>
+                    <UploadForm/>
+                    {/* <UploadedProductsDisplay products={this.state.products}/> */}
+                </div>
+            )
+        }
     }
 }
