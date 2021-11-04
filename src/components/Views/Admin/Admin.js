bed9e71926e54256ea05ac7703bf74f3083e6683
chrissyy99 <43761702+IslipAI@users.noreply.github.com>
2021-11-04 19:42:59 -0400

    Added daily and yearly sales charts.

--- src/components/Views/Admin/Admin.js
+++ src/components/Views/Admin/Admin.js
@@ -1,5 +1,6 @@
 import React, {PureComponent} from 'react';
 import { BarChart, Bar, LineChart, Line, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import '../Admin/Admin.css';
 
 
 /**
@@ -8,21 +9,21 @@ import { BarChart, Bar, LineChart, Line, Cell, XAxis, YAxis, CartesianGrid, Tool
  * @param {*} props pass through our chart data.
  * @returns Graph display.
  */
-function ChartsWeekly(props){
+function Chart(props){
     console.log(props.salesData);
     return(
-        <div width="100%" height="100%">
-        <select className="timeline" id="timeline" onChange={props.setChartTimeLine} defaultValue={"weekly"}>
+        <div className="chart-container">
+        <select className="timeline" id="timeline" onChange={props.setChartTimeLine}>
             <option value="daily">Daily</option>
             <option value="weekly">Weekly</option>
             <option value="yearly">Yearly</option>
         </select>
-        <BarChart width={600} height={300} data={props.salesData}>
+        <BarChart width={700} height={300} data={props.salesData}>
           <Bar dataKey="productSold" fill="black" />
           <XAxis dataKey="name" />
           <YAxis dataKey="productSold"/>
         </BarChart>
-        <LineChart width={600} height={300} data={props.salesData}>
+        <LineChart width={700} height={300} data={props.salesData}>
             <Line dataKey="totalSales" fill="black" />
             <XAxis dataKey="name" />
             <YAxis dataKey="totalSales"/>
@@ -34,22 +35,30 @@ function ChartsWeekly(props){
 function Charts(props){
     if(props.chartTimeLine === "daily"){
         return(
-            <div className="admin-wrapper">
-                <h3>Daily</h3>
+            <div className="salesChart">
+                <Chart 
+                salesData={props.dailySalesData} 
+                setChartTimeLine={props.setChartTimeLine}
+                />
             </div>
         );
     }else if(props.chartTimeLine === "weekly"){
         return (
-            <div className="admin-wrapper">
-                <ChartsWeekly 
-                    salesData={props.salesData} 
-                    setChartTimeLine={props.setChartTimeLine}
+            <div className="salesChart">
+                <Chart 
+                salesData={props.weeklySalesData} 
+                setChartTimeLine={props.setChartTimeLine}
                 />
             </div>
         );
     }else{
         return (
-            <h3>Yearly</h3>
+            <div className="salesChart">
+                <Chart 
+                salesData={props.yearlySalesData} 
+                setChartTimeLine={props.setChartTimeLine}
+                />
+            </div>
         );
     }
 }
@@ -59,19 +68,49 @@ export default class Admin extends PureComponent {
         super();
         this.state = {
           isLoaded: false,
-          salesData: null,
-          chartTimeLine: "weekly"
+          dailySalesData: null,
+          weeklySalesData: null,
+          yearlySalesData: null,
+          chartTimeLine: "daily"
         }
         this.setChartTimeLine = this.setChartTimeLine.bind(this);
       }
 
-    async fetchProductsSoldWeekly(){
+    async fetchSalesData(){
         await fetch('https://www.beatcaveapi.com/admin/weeklysales/')
         .then(res => res.json())
         .then((result) => {
             this.setState({
                 isLoaded: true,
-                salesData: result.elements
+                weeklySalesData: result.elements
+            });
+            },(error) =>{
+                this.setState({
+                    isLoaded: true, 
+                    error
+                });
+            }
+        )
+        await fetch('https://www.beatcaveapi.com/admin/yearlysales/')
+        .then(res => res.json())
+        .then((result) => {
+            this.setState({
+                isLoaded: true,
+                yearlySalesData: result.elements
+            });
+            },(error) =>{
+                this.setState({
+                    isLoaded: true, 
+                    error
+                });
+            }
+        )
+        await fetch('https://www.beatcaveapi.com/admin/dailysales/')
+        .then(res => res.json())
+        .then((result) => {
+            this.setState({
+                isLoaded: true,
+                dailySalesData: result.elements
             });
             },(error) =>{
                 this.setState({
@@ -84,21 +123,29 @@ export default class Admin extends PureComponent {
 
     setChartTimeLine(event){
         //console.log(event.target.value)
-
         this.setState({
             chartTimeLine: event.target.value,
         });
     }
 
     componentDidMount(){
-        this.fetchProductsSoldWeekly();
+        this.fetchSalesData();
     }
 
   render() {
-    const {salesData, isLoaded, chartTimeLine} = this.state;
+    const {dailySalesData, weeklySalesData, yearlySalesData, isLoaded, chartTimeLine} = this.state;
     if(isLoaded){
         return(
-            <Charts salesData={salesData} chartTimeLine={chartTimeLine} setChartTimeLine={this.setChartTimeLine}/>
+            <div className="admin-wrapper">
+                <h1>Admin Panel</h1>
+                <Charts 
+                dailySalesData={dailySalesData}
+                weeklySalesData={weeklySalesData} 
+                yearlySalesData={yearlySalesData}
+                chartTimeLine={chartTimeLine}
+                setChartTimeLine={this.setChartTimeLine}
+            />
+            </div>
         )
     }else{
         return(
