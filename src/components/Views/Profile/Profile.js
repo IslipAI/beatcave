8d901f0181ededaf2b5cf3535d94b222979f1eed
chrissyy99 <43761702+IslipAI@users.noreply.github.com>
2021-11-14 22:48:11 -0500

    Added profile update feature.

--- src/components/Views/Profile/Profile.js
+++ src/components/Views/Profile/Profile.js
@@ -1,47 +1,136 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import '../Profile/Profile.css';
 import profilepicture from '../../Images/blank-profile-picture.png';
+import {uploadFile} from 'react-s3';
 
-//function returns the user display populated with user data
+/**
+ * Function returns user data display. Allows for new 
+ * profile picture selection and profile update.
+ * @param {*} props 
+ * @returns html user display.
+ */
 function UserDisplay(props){
+
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileInput = (e) => {
+        setSelectedFile(e.target.files[0]);
+    }
+
+    //console.log(props.profilepicturepath)
     return(
         <div className="profile-display">
             <h1 className="profile-view-title">Profile</h1>
-            <img className="profile-profile-picture" alt="Profile picture" src='https://beatcave.s3.us-east-2.amazonaws.com/profilepictures/ProfilePic.jpg'/>
+            <img className="profile-profile-picture" alt="Profile picture" src={props.profilepicturepath}/>
             <br/>
-            <input type="text" defaultValue={props.email}/>
+            <input 
+                type="file" 
+                onChange={handleFileInput}
+                className="fileInput"
+            />
+            <br/>
+            <input 
+                type="text" 
+                defaultValue={props.email} 
+                disabled
+            />
              <br/>
-             <input type="text" defaultValue={props.firstname}/>
+             <input 
+                type="text" 
+                defaultValue={props.firstname}
+                name="firstname"
+                onChange={props.handleChange}
+             />
              <br/>
-             <input type="text" defaultValue={props.lastname}/>
+             <input 
+                type="text" 
+                defaultValue={props.lastname}
+                name="lastname"
+                onChange={props.handleChange}
+             />
              <br/>
-             <input type="text" defaultValue={props.artistname}/>
+             <input 
+                type="text" 
+                defaultValue={props.artistname}
+                name="artistname"
+                onChange={props.handleChange}
+             />
              <br/>
-             <input type="text" defaultValue={props.phonenumber}/>
+             <input 
+                type="text" 
+                defaultValue={props.phonenumber}
+                name="phonenumber"
+                onChange={props.handleChange}
+             />
              <br/>
-             <input type="text" defaultValue={props.birthdate.split('T')[0]}/>
+             <input 
+                type="text" 
+                defaultValue={props.birthdate.split('T')[0]}
+                disabled
+             />
+             <br/>
+             <button type="button" onClick={() => props.handleUpload(selectedFile)}>Save</button>
         </div>
     )
 }
 
-function updateUser(){
+/**
+ * Function sends put request to update user.
+ * @param {*} id - users id
+ * @param {*} firstname -users firstname
+ * @param {*} lastname -users lastname
+ * @param {*} artistname - users artistname
+ * @param {*} phonenumber - users phonenumber
+ * @param {*} profilepicturepath - users profile picture path
+ */
+async function updateUser(id, firstname, lastname, artistname, phonenumber, profilepicturepath){
+    const requestOptions = {
+        method: 'PUT',
+        headers: 
+        { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ 
+            id: id,
+            firstname: firstname,
+            lastname: lastname,
+            artistname: artistname,
+            phonenumber: phonenumber,
+            profilepicturepath: profilepicturepath
+        })
+      }
 
+      await fetch('http://localhost:8000/users/updateuser/', requestOptions)
+        .then(response => console.log(response))
+        .catch(error => {
+            console.log(error)
+          }
+        )
 }
 
 export default class Profile extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          firstname: "",
-          lastname: "", 
-          email: "",
-          artistname: "",
-          phonenumber: "",
-          birthdate: ""
+            id: null,
+            firstname: "",
+            lastname: "", 
+            email: "",
+            artistname: "",
+            phonenumber: "",
+            birthdate: "",
+            profilepicturepath: "",
+            bucket: '',
+            directory: 'profilepictures',
+            region: '',
+            access: '',
+            secret: '',
         };
+        this.handleChange = this.handleChange.bind(this);
       }
 
-    //Calls API to get user data by id
+    /**
+     * Function gets user data.
+     * @param {*} id - users id.
+     */
     async fetchUser(id){
         await fetch("https://www.beatcaveapi.com/users/user/" + id.toString() + "/")
         .then(response => {
@@ -55,7 +144,8 @@ export default class Profile extends Component {
                 lastname: data.elements[0].lastname,
                 artistname: data.elements[0].artistname,
                 phonenumber: data.elements[0].phonenumber,
-                birthdate: data.elements[0].birthdate
+                birthdate: data.elements[0].birthdate,
+                profilepicturepath: data.elements[0].profilepicturepath
             })
             return data;
           }).catch(
@@ -65,12 +155,32 @@ export default class Profile extends Component {
           )
     }
 
+    /**
+     * Function retrives AWS bucket data.
+     */
     async fetchAWS(){
-
+        await fetch('https://www.beatcaveapi.com/users/user/aws/access', {mode: 'cors'},)
+        .then((response) => {
+            return response.json();
+        })
+        .then((result)=>{
+            //console.log(result);
+            this.setState({
+                bucket: result.elements[0].bucketname,
+                //directory: result.elements[0].directory,
+                region: result.elements[0].region,
+                access: result.elements[0].access,
+                secret: result.elements[0].private,
+            });
+            //console.log(this.state.directory);
+        })
     }
 
 
-    //Returns stored user id.
+    /**
+     * Function gets userID from JWT
+     * @returns - user id
+     */
     getUserId(){
         const storedToken = sessionStorage.getItem('token');
         if(storedToken != null){
@@ -83,12 +193,68 @@ export default class Profile extends Component {
         }
     }
 
+    /**
+     * ComponentsDidMount method prepares all
+     * data before render.
+     */
     componentDidMount(){
         var id = this.getUserId();
-        console.log(id)
+        this.setState({
+            id: id
+        });
+        //console.log(id)
         this.fetchUser(id);
+        this.fetchAWS();
     }
 
+    /**
+     * Function handles file upload. If file is null, then 
+     * it will only update the sql data and now aws bucket.
+     * @param {*} file - users new profile picture.
+     */
+    handleUpload = async (file) => {
+        const {id, firstname, lastname, 
+            artistname, phonenumber, 
+            profilepicturepath, bucket, 
+            directory, region, 
+            access, secret} = this.state;
+
+        //S3 UPLOAD CONFIG.
+        const config = {
+            bucketName: bucket,
+            dirName: directory,
+            region: region,
+            accessKeyId: access, 
+            secretAccessKey: secret,
+        }
+
+        if(file == null){
+            updateUser(id, firstname, lastname, artistname, phonenumber, profilepicturepath)
+        }else{
+            uploadFile(file, config)
+            .then(async function(data){
+                //console.log(data.location);
+                updateUser(id, firstname, lastname, artistname, phonenumber, data.location)
+            })
+            .catch(err => console.error(err))
+        }
+    }
+
+    /**
+     * Changes variables value on event change.
+     * @param {*} event - user changing input.
+     */
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    }
+
+
+    /**
+     * Components render 
+     * @returns User display
+     */
     render() {
         var email = this.state.email;
         var firstname = this.state.firstname;
@@ -96,6 +262,7 @@ export default class Profile extends Component {
         var artistname = this.state.artistname;
         var phonenumber = this.state.phonenumber;
         var birthdate = this.state.birthdate;
+        var profilepicturepath = this.state.profilepicturepath;
 
         return (
             <div className="profile-wrapper">
@@ -106,6 +273,9 @@ export default class Profile extends Component {
                     artistname={artistname}
                     phonenumber={phonenumber}
                     birthdate={birthdate}
+                    profilepicturepath={profilepicturepath}
+                    handleUpload={this.handleUpload}
+                    handleChange={this.handleChange}
                 />
             </div>
         )
